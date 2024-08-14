b9dd9caba8f43d9ac84d419cf2c9c93039dd63bd
Joy Pahan <143921667+ProgrammingWithJoyPahan@users.noreply.github.com>
2024-08-14 23:06:33 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(GreetingApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GreetingApp extends StatelessWidget {
+  const GreetingApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Extended Greeting App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Greeting App'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'Hello, World!',
-                style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
-              ),
-              const Text('Welcome to Flutter!'),
+      title: "Greeting App",
+      home: HomeActivity(),
+    );
+  }
+}
 
-              Image.network('https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/flutter-icon.png'),
-              ElevatedButton(
-                onPressed: () {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Button Pressed!')),
-                  );
-                },
-                child: const Text('Press Me'),
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-              ),
-            ],
-          ),
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+    MySnakeBar(message,context){
+      return ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(message))
+      );
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Greeting App'),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello, World!',style: TextStyle(color: Colors.red,fontSize: 18, fontWeight: FontWeight.bold),),
+            SizedBox(height: 16,),
+            Text('Welcome to Flutter'),
+            SizedBox(height: 16,),
+            Image.network('https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/flutter-icon.png',height: 200,),
+            SizedBox(height: 16,),
+            ElevatedButton(onPressed: (){
+              MySnakeBar('Button Pressed', context);
+            }, child: Text('Press Me'),style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.green,
+            ),),
+          ],
         ),
       ),
     );
